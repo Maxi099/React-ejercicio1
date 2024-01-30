4968a52ab39e404b644273381c79c1890e4e5b18
Maxi <ixam033@gmail.com>
2024-01-29 21:07:26 -0300

    Version Final2

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -241,7 +241,7 @@ export const Message = styled.div`
     top:0;
     bottom:0;
     width:100%;
-    height:100%;
+    height:100vh;
     background-color: white;
     opacity:0.9;
     z-index:100;
@@ -252,6 +252,10 @@ export const Message = styled.div`
     gap:20px;
     text-align:center;
     padding:0 10px;
+    @media (max-width: 768px) {
+        height:100dvh;
+      }
+
 p{
     font-weight:bold;
 }
